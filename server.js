f873abc78df0d19e5e1bcc36996a1d89cace0540
Avani2592 <avani2592@gmail.com>
2026-02-06 14:56:53 -0500

    testing endpoint fix package.json

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const app=require('./app');
 const PORT=process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Azure Node app is running ✅');
+});
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
